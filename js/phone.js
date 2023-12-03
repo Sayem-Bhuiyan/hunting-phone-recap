67ea96cfd054afb31ee13dea7f64353ab9aa8b6c
Sayem <sayem465508@gmail.com>
2023-12-03 20:09:58 +0600

    added show details from api

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText='iphone', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phone = data.data;
@@ -16,7 +16,6 @@ const displayPhone = (phones, isShowAll) => {
     else{
         showAllBtn.classList.add('hidden')
     }
-    console.log('show all btn', isShowAll)
     if(!isShowAll){
         phones =  phones.slice(0, 12)
     }
@@ -32,7 +31,7 @@ const displayPhone = (phones, isShowAll) => {
           <p>There are many variations of passages of available, but the majority have suffered</p>
           <p class="text-3xl font-semibold">$999</p>
           <div class="">
-            <button class="btn btn-primary">Show Details</button>
+            <button onclick="show_phone_details.showModal(); showPhoneDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
       </div>
@@ -64,3 +63,44 @@ const toggoleLoadingSpinner = (isLoading) => {
         loadingSpinner.classList.add('hidden')
     }
 }
+
+const showPhoneDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    const phone = data.data;
+    displayPhoneDetails(phone)
+}
+
+
+const displayPhoneDetails = (phone) => {
+    console.log(phone)
+    const detailsContainer = document.getElementById('show-details-container');
+    detailsContainer.innerHTML = `
+    
+        <img src="${phone.image}" />
+        <div class="space-y-5 mt-10">
+            <h2><span class="text-3xl font-semibold">${phone.name}</span></h2>
+
+            <p class="text-base">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+
+            <p><span class="text-2xl font-medium">Storage: </span>${phone?.mainFeatures?.storage}</p>
+
+            <p><span class="text-2xl font-medium">Display Size: </span>${phone?.mainFeatures?.displaySize}</p>
+
+            <p><span class="text-2xl font-medium">Chipset: </span>${phone?.mainFeatures?.chipSet}</p>
+
+            <p><span class="text-2xl font-medium">Memory: </span>${phone?.mainFeatures?.memory}</p>
+
+            <p><span class="text-2xl font-medium">Slug: </span>${phone?.slug}</p>
+
+            <p><span class="text-2xl font-medium">Release Date: </span>${phone?.releaseDate}</p>
+
+            <p><span class="text-2xl font-medium">Brand: </span>${phone?.brand}</p>
+
+            <p><span class="text-2xl font-medium">GPS: </span>${phone?.others?.GPS}</p>
+        </div>
+    
+    `
+}
+// showPhoneDetails()
+loadPhone()
