1c5898abff0958e5f012e16c76f06c5aeeea07c0
Sayem <sayem465508@gmail.com>
2023-12-03 19:12:36 +0600

    create the ui of phone hunter api

---
+++ js/phone.js
@@ -0,0 +1,53 @@
+const loadPhone = async (searchText, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json()
+    const phone = data.data;
+    displayPhone(phone, isShowAll)
+}
+
+const displayPhone = (phones, isShowAll) => {
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = "";
+
+    const showAllBtn = document.getElementById('show-all-btn')
+    if(phones.length > 12 && !isShowAll){
+        showAllBtn.classList.remove('hidden')
+    }
+    else{
+        showAllBtn.classList.add('hidden')
+    }
+    console.log('show all btn', isShowAll)
+    if(!isShowAll){
+        phones =  phones.slice(0, 12)
+    }
+
+    phones.forEach((phone) => {
+        const phoneCard = document.createElement('div');
+        phoneCard.innerHTML = `
+        
+        <div class="card p-8 bg-base-100 shadow-xl text-center">
+        <figure><img src="${phone.image}" alt="phone image" /></figure>
+        <div class=" flex flex-col space-y-5 ">
+          <h2 class="text-2xl font-semibold mt-5">${phone.phone_name}</h2>
+          <p>There are many variations of passages of available, but the majority have suffered</p>
+          <p class="text-3xl font-semibold">$999</p>
+          <div class="">
+            <button class="btn btn-primary">Show Details</button>
+          </div>
+        </div>
+      </div>
+
+        `;
+        phoneContainer.appendChild(phoneCard)
+    });
+}
+
+const handleSearch = (isShowAll) => {
+    const serachField = document.getElementById('search-field');
+    const searchText = serachField.value;
+    loadPhone(searchText, isShowAll)
+}
+
+const handleShowAll = () => (
+    handleSearch(true)
+)
